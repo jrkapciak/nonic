c407e6004ca99e58393abf2df0bb2bee0c242972
jrkapciak <jerzyryszardkapciak@gmail.com>
2022-10-03 19:07:02 +0200

    add drf-yasg

--- nonic/urls.py
+++ nonic/urls.py
@@ -14,10 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Nonic API",
+        default_version="v1",
+        contact=openapi.Contact(email="jerzyryszardkapciak@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-mobile/", include(("api_mobile.urls", "api-mobile"), namespace="api-mobile")),
     path("users/", include(("users.urls", "users"), namespace="users")),
 ]
+
+urlpatterns += (re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),)
